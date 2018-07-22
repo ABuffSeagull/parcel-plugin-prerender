02a94121b73e1f60fe7bf284ac92560ce8a10609
ABuffSeagull <reecevanatta@gmail.com>
2018-07-22 02:51:23 -0400

    Add cosmiconfig
    As well as move some things around to improve perfomance
    of non-builds

--- index.js
+++ index.js
@@ -2,39 +2,44 @@ const fs = require('fs');
 const path = require('path');
 const mkdirp = require('mkdirp');
 const chalk = require('chalk');
+const cosmiconfig = require('cosmiconfig');
 const Prerenderer = require('@prerenderer/prerenderer');
 const Puppeteer = require('@prerenderer/renderer-puppeteer');
 
 module.exports = bundler => {
-  const { outDir } = bundler.options;
-  const prerenderer = new Prerenderer({
-    staticDir: outDir,
-    renderer: new Puppeteer(),
-  });
   bundler.on('buildEnd', async () => {
     if (process.env.NODE_ENV !== 'production') return;
+    const { outDir } = bundler.options;
+    const prerenderer = new Prerenderer({
+      staticDir: outDir,
+      renderer: new Puppeteer(),
+    });
+    const explorer = cosmiconfig('prerender');
+    const {
+      config: { routes },
+    } = (await explorer.search()) || { config: { routes: ['/'] } };
     console.log(chalk.bold('\nRendering...'));
     try {
-      await prerenderer.initialize()
+      await prerenderer.initialize();
       const start = new Date().getTime();
-      const renderedRoutes = await prerenderer.renderRoutes(['/', '/test']);
+      const renderedRoutes = await prerenderer.renderRoutes(routes);
       const end = new Date().getTime();
-  		renderedRoutes.forEach(route => {
-    		try {
-					const outputDir = path.join(outDir, route.route);
-					const file = path.normalize(`${outputDir}/index.html`);
-					mkdirp.sync(outputDir);
-					fs.writeFileSync(file, route.html.trim());
-					const end = new Date().getTime();
-    		} catch (err) {
-      		console.error(err);
-    		}
-  		});
-			console.log(`Finished rendering in ${end - start}ms.`);
-  		prerenderer.destroy();
-		} catch (err) {
-  		prerenderer.destroy();
-  		console.error(err);
-		}
+      renderedRoutes.forEach(route => {
+        try {
+          const outputDir = path.join(outDir, route.route);
+          const file = path.normalize(`${outputDir}/index.html`);
+          mkdirp.sync(outputDir);
+          fs.writeFileSync(file, route.html.trim());
+          const end = new Date().getTime();
+        } catch (err) {
+          console.error(err);
+        }
+      });
+      console.log(`Finished rendering in ${end - start}ms.`);
+      prerenderer.destroy();
+    } catch (err) {
+      prerenderer.destroy();
+      console.error(err);
+    }
   });
-}
+};
